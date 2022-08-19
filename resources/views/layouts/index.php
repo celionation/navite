5481080859f594efce16ea37661971c40c1e35d2
celio natti <celionatti@gmail.com>
2022-08-19 22:50:39 +0100

    initial commit, added tailwind css and bootstrap

---
+++ resources/views/layouts/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/resources/css/tailwind.css">
+    <title>Navite PHP MVC Framework</title>
+</head>
+
+<body>
+    <h1 class="text-blue-900 font-bold text-5xl">Navite Framework</h1>
+    <p class="bg-purple-600 px-4 py-2 text-white hover:bg-blue-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi officiis corporis assumenda incidunt voluptatem cupiditate aperiam temporibus et repellat similique!</p>
+</body>
+
+</html>
